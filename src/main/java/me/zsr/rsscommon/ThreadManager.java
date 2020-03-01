dea37d55dacfb3fc23620164350ea8d8ac2ea483
zsr <shaoru.zsr@alibaba-inc.com>
2020-03-01 21:01:27 +0800

    thread pool optimize

--- src/main/java/me/zsr/rsscommon/ThreadManager.java
+++ src/main/java/me/zsr/rsscommon/ThreadManager.java
@@ -3,10 +3,14 @@ package me.zsr.rsscommon;
 import android.os.Handler;
 import android.os.Looper;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ThreadManager {
     private static Handler mMainThreadHandler;
 
     private static Handler mBackgroundHandler;
+    private static ExecutorService mExecutorService;
 
     public static void init() {
         mMainThreadHandler = new Handler();
@@ -18,6 +22,8 @@ public class ThreadManager {
                 Looper.loop();
             }
         }).start();
+
+        mExecutorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() + 2);
     }
 
     public static void post(Runnable runnable) {
@@ -28,7 +34,14 @@ public class ThreadManager {
         mMainThreadHandler.postDelayed(runnable, delay);
     }
 
+    /**
+     * for simple, thread unsafe runnable
+     */
     public static void postInBackground(Runnable runnable) {
         mBackgroundHandler.post(runnable);
+    }
+
+    public static void execute(Runnable runnable) {
+        mExecutorService.execute(runnable);
     }
 }
